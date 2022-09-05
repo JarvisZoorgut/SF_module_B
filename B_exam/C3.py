d081f792f211d8dbf258bded5b5ee201f42768d5
JarvisZoorgut <111298417+JarvisZoorgut@users.noreply.github.com>
2022-09-05 17:22:15 +0500

    23.06.2022

---
+++ B_exam/C3.py
@@ -0,0 +1,6 @@
+import os
+import math
+
+print(os.getcwd())  # получить текущую директорию
+print(os.listdir())  # получить список файлов текущей директории
+help(math)
---
+++ B_exam/main.py
@@ -0,0 +1,10 @@
+import time
+
+i = 11
+while i != 0:
+    print(i-1)
+    time.sleep(1)
+    i -= 1
+
+print('Время вышло')
+
