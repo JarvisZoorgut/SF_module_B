155ec25bf6668ae788daed9147218fa5ce174572
JarvisZoorgut <111298417+JarvisZoorgut@users.noreply.github.com>
2022-09-21 22:13:58 +0500

    21.09.22 - C5.bot.final

--- bot_1/bot_1_app.py
+++ bot_1/bot_1_app.py
@@ -1,54 +1,14 @@
+import datetime
 import telebot
-import requests
-import json
-import extensions
-
-TOKEN = '5544590468:AAEpu0fgS7s4SXQMWShGxKJ75yG0LmbFU-I'
+from config import keys, TOKEN
+from extensions import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-class ConvertionException(Exception):
-        pass
-
-class CryptoConverter:
-        @staticmethod
-        def convert(quote: str, base: str, amount: str):
-                if quote == base:
-                        raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-
-                try:
-                        quote_ticker = keys[quote]
-                except KeyError:
-                        raise ConvertionException(f'Не удалось обработать валюту {quote}.')
-
-                try:
-                        base_ticker = keys[base]
-                except KeyError:
-                        raise ConvertionException(f'Не удалось обработать валюту {base}.')
-
-                try:
-                        amount = float(amount)
-                except ValueError:
-                        raise ConvertionException(f'Не удалось обработать количество {amount}.')
-
-                url = f"https://api.apilayer.com/exchangerates_data/convert?to={base_ticker}&from={quote_ticker}&amount={amount}"
-                payload = {}
-                headers = {
-                        "apikey": "J2vlt90OZJ5QG6huCpj5k6wHmv1XKYqx"
-                }
-                r = requests.request("GET", url, headers=headers, data=payload)
-                total_base = json.loads(r.content)['result']
-                return total_base
-
-keys = {
-        'доллар': 'USD',
-        'евро': 'EUR',
-        'рубль': 'RUB'
-}
-
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-        text = 'Чтобы начать работу, введите команду в следующем формате:\n<имя валюты на русском>\n \
+        text = 'Чтобы начать работу, введите команду с маленькой буквы в одну строку в следующем формате:\n \
+<имя валюты на русском (доллар, евро и т.д.)\n или большими буквами тикеров (USD, EUR и т.д.)>\n \
 <в какую валюту перевести>\n \
 <количество переводимой валюты>\n\nУвидеть список доступных валют:\n /values '
         bot.reply_to(message, text)
@@ -62,14 +22,20 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-        values = message.text.split(' ')
-        if len(values) != 3:
-                raise ConvertionException('Слишком много параметров')
+        try:
+                values = message.text.split(' ')
+                if len(values) != 3 or values == keys['или тикером']:
+                        raise ConvertionException('Неверный ввод\n\nПомошь: /help')
 
-        quote, base, amount = values
-        total_base = CryptoConverter.convert(quote, base, amount)
-
-        text = f'Цена {amount} {quote} в {base} = {total_base}'
-        bot.send_message(message.chat.id, text)
+                quote, base, amount = values
+                total_base = CryptoConverter.convert(quote, base, amount)
+        except ConvertionException as e:
+                bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+        except Exception as e:
+                bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+        else:
+                dt = (datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+                text = f'На {dt}\nЦена {amount} {quote} = {total_base:.2f} {base} '
+                bot.send_message(message.chat.id, text)
 
 bot.polling()
---
+++ bot_1/config.py
@@ -0,0 +1,16 @@
+TOKEN = '5544590468:AAEpu0fgS7s4SXQMWShGxKJ75yG0LmbFU-I'
+
+keys = {
+        'доллар': 'USD',
+        'евро': 'EUR',
+        'рубль': 'RUB',
+        'юань': 'CNY',
+        'биткоин': 'BTC',
+        '':'',
+        'или тикером': '',
+        'USD': 'USD',
+        'EUR': 'EUR',
+        'RUB': 'RUB',
+        'CNY': 'CNY',
+        'BTC': 'BTC'
+}
